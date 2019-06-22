a8deec0ee12acb7f99c26ff5679819ec8094c7bc
Jose Cordero <joscorbe@hotmail.com>
2019-06-22 11:33:24 +0200

    Scooters and Trains endpoints and database tables.

--- api/scooter.py
+++ api/scooter.py
@@ -1,13 +1,20 @@
 
-from flask import Blueprint
+import os
+import boto3
+from flask import Blueprint, jsonify
 
 app = Blueprint('scooter', __name__, url_prefix='/scooter')
+dynamo_client = boto3.client('dynamodb', region_name='eu-central-1')
 
-
-@app.route('lock', methods=['POST'])
+@app.route('lock/<string:scooter_id>', methods=['POST'])
 def enter():
     return 'Scooter locked'
 
-@app.route('unlock', methods=['POST'])
+@app.route('unlock/<string:scooter_id>', methods=['POST'])
 def exit():
     return 'Scooter unlocked'
+
+@app.route('read', methods=['GET'])
+def read():
+    return jsonify({'scooters': dynamo_client.scan(TableName='scooter-table')})
+
--- api/train.py
+++ api/train.py
@@ -1,13 +1,19 @@
 
-from flask import Blueprint
+import os
+import boto3
+from flask import Blueprint, jsonify
 
 app = Blueprint('train', __name__, url_prefix='/train')
+dynamo_client = boto3.client('dynamodb', region_name='eu-central-1')
 
-
-@app.route('enter', methods=['POST'])
+@app.route('enter/<string:train_id>', methods=['POST'])
 def enter():
     return 'You are in!'
 
-@app.route('exit', methods=['POST'])
+@app.route('exit/<string:train_id>', methods=['POST'])
 def exit():
     return 'Trip recorded'
+
+@app.route('read', methods=['GET'])
+def read():
+    return jsonify({'trains': dynamo_client.scan(TableName='train-table')})
