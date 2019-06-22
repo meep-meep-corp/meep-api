cfd676378ee0aac6da1fefa6e2b64387ced1d394
Jose Andres Cordero <joscorbe@hotmail.com>
2019-06-22 21:01:09 +0200

    Fixed CORS issue

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,11 @@
 
 from flask import Flask
 from api import vehicle, trip
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(vehicle.app)
     app.register_blueprint(trip.app)
     # app.register_error_handler(Exception, handle_error)
